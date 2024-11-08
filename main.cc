a8d1eb59fa953653a425762dd3e0000b58cb276e
FreezB11 <yashrajrao4@gmail.com>
2024-11-09 04:58:01 +0530

    learning

--- main.cc
+++ main.cc
@@ -1,55 +1,12 @@
-#include <iostream>
-#include "src/ark.h"
-#include "src/matrix.h"
-
-void printMatrix(const Matrix& matrix, const std::string& channelName) {
-    std::cout << channelName << " Matrix:\n";
-    for (int i = 0; i < matrix.rows; ++i) {
-        for (int j = 0; j < matrix.cols; ++j) {
-            std::cout << matrix.data[i][j] << " ";
-        }
-        std::cout << "\n";
-    }
-    std::cout << "\n";
-}
-
-// Helper function to print the DFT result
-void printDFT(const std::vector<std::vector<std::complex<float>>>& dftMatrix, const std::string& channelName) {
-    std::cout << channelName << " Frequency Matrix (Magnitude):\n";
-    for (int i = 0; i < dftMatrix.size(); ++i) {
-        for (int j = 0; j < dftMatrix[i].size(); ++j) {
-            std::cout << std::abs(dftMatrix[i][j]) << " ";
-        }
-        std::cout << "\n";
-    }
-    std::cout << "\n";
-}
+// @file: main.cc
+// @author: hsay
+#include <eigen3/Eigen/Dense>
 
 int main() {
-    std::string imagePath = "girl.png";  // Replace with the path to your image file
-
-    // Initialize CNN and matrices
-    CNN cnn;
-    Matrix red(0, 0), green(0, 0), blue(0, 0);
-
-    // Load the image and split into RGB matrices
-    if (cnn.loadImage(imagePath, red, green, blue)) {
-        // Print the matrices
-        // printMatrix(red, "Red");
-        // printMatrix(green, "Green");
-        // printMatrix(blue, "Blue");
-        // Compute and print the DFT of each color channel
-        auto redDFT = red.computeDFT();
-        auto greenDFT = green.computeDFT();
-        auto blueDFT = blue.computeDFT();
-
-        // printDFT(redDFT, "Red");
-        // printDFT(greenDFT, "Green");
-        // printDFT(blueDFT, "Blue");
-
-    } else {
-        std::cerr << "Failed to load the image: " << imagePath << "\n";
-    }
+    Eigen::MatrixXd m(3,3);
+    m << 1,2,3,
+         4,5,6,
+         7,8,9;
 
     return 0;
 }
--- makefile
+++ makefile
@@ -1,20 +1,6 @@
 CC = g++
-FLAGS = -o 
-IFLAGS = -lm
-FILES = main.cc src/*.cc src/lib/math/*.cc #lib/nn.c lib/math/calculas.c
-# OBJ_FILES = main.o 
-OUT_DIR = builds/main
-# gcc -o builds/main main.c
-#gcc nn.c lib/calculas.c -lm (for math lib) 
-# to create obj files
-# gcc -Wall -c {filename}.c
+lib = -LEigen #-lieutenant
+out = main
 
-nn: ${FILES}
-	@echo "🚧 Building..."
-	${CC} ${FLAGS} ${OUT_DIR} ${FILES} ${IFLAGS}
-
-run: ${OUT_DIR}
-	./${OUT_DIR}
-
-print: $(wildcard *.c)
-	ls -la  $?
+all:
+	${CC} -o ${out} main.cc ${lib}
