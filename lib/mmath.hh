928e65e819c76db8d252cc1222fa1ed735fd15d9
FreezB11 <yashrajrao4@gmail.com>
2024-12-31 02:07:29 +0530

    activations

--- lib/mmath.hh
+++ lib/mmath.hh
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <eigen3/Eigen/Dense>
+#include <math.h>
 
 Eigen::MatrixXf convolve(Eigen::MatrixXf base, Eigen::MatrixXf kernel,int padding = 0, int stride = 1){
     int base_rows = base.rows();
@@ -13,4 +14,31 @@ Eigen::MatrixXf convolve(Eigen::MatrixXf base, Eigen::MatrixXf kernel,int paddin
 
     */
 
-} 
+}
+
+// Activation functions
+
+double sigmoid(double x){
+    return (exp(x)/( 1 + exp(x)));
+}
+
+double binary_step(double c){
+    if( c >= 0){
+        return 1.0f;
+    }else{
+        return 0.0f;
+    }
+}
+
+double tanh(double x){
+    double t = exp(2*x);
+    return ((t-1)/(t+1));
+}
+
+double relu(double x){
+    if( x<= 0.0f){
+        return 0.0f;
+    }else{
+        return x;
+    }
+}
